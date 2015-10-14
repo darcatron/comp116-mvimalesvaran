db15dad4dc9e28e4c0d9767b88e4135b6c07dd3f
Mathurshan Vimalesvaran <Mathurshan.Vimalesvaran@tufts.edu>
2015-10-13 23:02:26 -0400

    parsing works and vulnurabilities are detected

--- alarm/alarm.rb
+++ alarm/alarm.rb
@@ -1,35 +1,12 @@
 require 'packetfu'
+require 'rubygems'
+# SOURCE: http://simonecarletti.com/blog/2009/02/apache-log-regex-a-lightweight-ruby-apache-log-parser/
+require 'apachelogregex'
+require 'date'
 
 USAGE_STRING = "Usage: #{$0} [-r] [logfile_path]"
 
 $vul_num = 0
-#{incident_number}. ALERT: #{incident} is detected from #{source IP address} (#{protocol}) (#{payload})!
-
-# SOURCE: http://snippets.aktagon.com/snippets/335-how-to-parse-apache-logs-with-ruby
-class ApacheLog
-  FORMATS = {
-    :combined => %r{^(\S+) - - \[(\S+ \+\d{4})\] "(\S+ \S+ [^"]+)" (\d{3}) (\d+|-) "(.*?)" "([^"]+)"$}
-  }
-  
-  class << self
-    def each_line(log_file, log_format = FORMATS[:combined])
-
-      f = File.open(log_file, "r")
-
-      f.each_line do|line|
-        data = line.scan(log_format).flatten
-
-        if data.empty?
-          p "Line didn't match pattern: #{line}"
-
-          next
-        end
-
-        yield data
-      end
-    end
-  end
-end
 
 def alert(vul, info, proto)
 	puts "#{$vul_num}. ALERT: #{vul} is deteced from #{info.ip} (#{proto}) (#{info.payload})!"
@@ -38,7 +15,6 @@ end
 
 def live_scan()
 	stream = PacketFu::Capture.new(:start => true, :iface => 'eth0', :promisc => true)
-	# stream.show_live()
 
 	stream.stream.each do |p|
 		pkt = PacketFu::Packet.parse p
@@ -63,14 +39,15 @@ def live_scan()
 				alert("XMAS scan", pkt, "TCP")
 			end
 		end
+		
 
-		# TODO: check other Nmap scans
-		if (pkt.payload =~ /Nmap/)
+		# check other Nmap scans
+		if (not pkt.payload.scan(/\x4e\x6d\x61\x70/).empty? || pkt.payload =~ /Nmap/)
 			alert("Other Nmap scan", pkt, protocol[-1])
 		end
 
-		# TODO: check Nikto scans
-		if (pkt.payload =~ /Nikto/)
+		# check Nikto scans
+		if (not pkt.payload.scan(/\x4e\x69\x6b\x74\x6f/).empty? || pkt.payload =~ /Nikto/)
 			alert("Nikto scan", pkt, protocol[-1])
 		end
 
@@ -84,37 +61,46 @@ def live_scan()
 end
 
 def log_scan(file)
-	# TODO: Q: is HTTP always the protocol for log files
-	# TODO: Q: do i need quotes for the payload ("GET HTTP/afjkafj/") vs (GET HTTPS/asfsa/fsa)
-	ApacheLog.each_line(file) do |data|
-		host, date, url_with_method, status, size, referrer, agent = data
-	# TODO: 
+	format = '%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-agent}i\"'
+	parser = ApacheLogRegex.new(format)
+
+	File.readlines(file).collect do |line|
+	  	data = parser.parse line
+
 		# NMAP scan (of any variety)
-		if (data =~ /Nmap/)
-			alert("Nmap scan", LogInfo.new(host, url_with_method), "HTTP")
+		if (line =~ /Nmap/)
+			alert("Nmap scan", LogInfo.new(data['%h'], data['%r']), "HTTP")
 		# Nikto scan
-		elsif (data =~ /Nikto/)
-			alert("Nikto scan", LogInfo.new(host, url_with_method), "HTTP")
+		elsif (line =~ /Nikto/)
+			alert("Nikto scan", LogInfo.new(data['%h'], data['%r']), "HTTP")
 		# Someone running Rob Graham's Masscan
+		elsif (line =~ /Masscan/)
+			alert("Masscan", LogInfo.new(data['%h'], data['%r']), "HTTP")
 		# Someone scanning for Shellshock vulnerability.
+		elsif (line =~ /{ :;}/)
+			alert("Shellshock", LogInfo.new(data['%h'], data['%r']), "HTTP")
 		# Anything pertaining to phpMyAdmin
+		elsif (line =~ /phpMyAdmin/)
+			alert("phpMyAdmin", LogInfo.new(data['%h'], data['%r']), "HTTP")
 		# Anything that looks like shellcode.
+		elsif (line =~ /\\x/)
+			alert("Shellcode", LogInfo.new(data['%h'], data['%r']), "HTTP")
 		end
 	end
 
 end
 
 if (ARGV[0] == "-r")
-	puts "WEB LOG ANALYZER "
-	LogInfo = Struct.new(:ip, :payload)
-
 	if (ARGV[1] == nil)
 		puts USAGE_STRING
 		exit(1)
 	end
 
+	LogInfo = Struct.new(:ip, :payload)
+
+	puts "Starting Web Log Analyzer..."
 	log_scan(ARGV[1])
 else
-	puts "LIVE SCAN"
+	puts "Starting Live Scan..."
 	live_scan()
 end
